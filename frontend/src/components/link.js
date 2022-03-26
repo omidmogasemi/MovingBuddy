3a25b92f7b61ae9ee289aca6401c74d4c2efbffe
Omid <omidmogasemi@gmail.com>
2022-03-26 11:09:06 -0700

    Add landing page

---
+++ frontend/src/components/link.js
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+import { jsx, NavLink as MenuLink, Link as A } from 'theme-ui';
+
+export function Link({ path, label, children, ...rest }) {
+  return (
+    <A {...rest} href={path}>
+      {children || label} 
+    </A>
+  );
+}
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import theme from 'theme';
+
+import SEO from 'components/seo';
+import Layout from 'components/layout';
+import Banner from '../sections/banner';
+import KeyFeature from '../sections/key-feature';
+
+export default function IndexPage() {
+  return (
+    <ThemeProvider theme={theme}>
+        <Layout>
+          <SEO title="NextJS Landing Page" />
+          <Banner />
+          <KeyFeature />
+        </Layout>
+    </ThemeProvider>
+  );
+}
